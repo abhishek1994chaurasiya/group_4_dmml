6a13f46579c4e22b2070eef39ec527314a1d2b61
abhishek1994chaurasiya <abhishek16chaurasiya@gmail.com>
2026-01-10 21:33:44 +0530

    Refactor DAG to include data generation and posting tasks; update file paths for CSV outputs

--- dags/reco_mart.py
+++ dags/reco_mart.py
@@ -1,11 +1,51 @@
+#############################################################
+# DAG: reco_data_pipeline
+# Description: End-to-end recommendation data pipeline
+# Schedule: Daily
+# Pre-requisites:
+#   - FastAPI server running on port 9000 (code/rest_api_setup.py)
+#   - MLflow server running on port 5000
+#############################################################
+
+from pathlib import Path
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 import logging
+from airflow.utils.task_group import TaskGroup
+import requests
+import papermill as pm
+
+
+code_path='/home/abhishek/Documents/Study/dmml_assignment/group_4_dmml/code/'
+raw_data_path='/home/abhishek/Documents/Study/dmml_assignment/group_4_dmml/data/source_raw/'
+
 
 # ------------------------
 # Task functions
 # ------------------------
+
+OUTPUT_DIR = Path("/tmp/notebook_outputs")
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
+
+def run_notebook(**context):
+    output_path = OUTPUT_DIR / f"train_model_{context['ds_nodash']}.ipynb"
+    pm.execute_notebook(
+        input_path=code_path +'SampleDataGenerator_v2.ipynb',
+        output_path=str(output_path),
+        parameters={
+            "run_date": context["ds"],
+        #     "input_path": "/data/input/"
+        }
+    )
+def post_data_function():
+    logging.info("Posting product data to REST API")
+    requests.post(
+        "http://127.0.0.1:9000/upload-dataset",
+        files={"file": open(raw_data_path + "products_dirty.csv", "rb")}
+    )
 
 def ingest_data():
     logging.info("Ingesting data from CSV and REST API")
@@ -29,26 +69,50 @@ with DAG(
     tags=["reco", "dmml", "assignment"],
 ) as dag:
 
-    ingest = PythonOperator(
-        task_id="ingest_data",
-        python_callable=ingest_data,
+    with TaskGroup(group_id="dataops_prework") as ingestion_group:
+        data_gen = PythonOperator(
+            task_id="data_generator_notebook",
+            python_callable=run_notebook,
+        )
+
+        upload_task = PythonOperator(
+            task_id="post_product_data",
+            python_callable=post_data_function,
+        )
+
+        data_gen >> upload_task
+
+
+    api_extract = BashOperator(
+        task_id="api_data_extract",
+        bash_command="python "+code_path+"api_data_extract.py",
+    )
+    csv_extract = BashOperator(
+        task_id="csv_data_extract",
+        bash_command="python "+code_path+"data_extract.py",
     )
 
-    transform = PythonOperator(
-        task_id="transform_data",
-        python_callable=transform_data,
-    )
-
-    train = PythonOperator(
-        task_id="train_model",
-        python_callable=train_model,
-    )
-
-    ingest >> transform >> train
+    ingest = ingestion_group >> [api_extract, csv_extract]
 
 
-#Plan schedule 
-1. code/rest_api_setup.py : Sets up a FastAPI server to upload and retrieve product data via REST API.
-2. code/api_data_extract.py : Fetches product data from the REST API and saves it
-3. code/data_extract.py : for interaction and users data into datalake
-4. 
+
+
+
+#     transform = PythonOperator(
+#         task_id="transform_data",
+#         python_callable=transform_data,
+#     )
+
+#     train = PythonOperator(
+#         task_id="train_model",
+#         python_callable=train_model,
+#     )
+
+#     ingest >> transform >> train
+
+
+# #Plan schedule 
+# 1. code/rest_api_setup.py : Sets up a FastAPI server to upload and retrieve product data via REST API.
+# 2. code/api_data_extract.py : Fetches product data from the REST API and saves it
+# 3. code/data_extract.py : for interaction and users data into datalake
+# 4. 
